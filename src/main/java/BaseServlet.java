a6d3e73dc6fb08d8fe8d1c1d7c0677893196235e
Hoa Pham <rubik0403@gmail.com>
2018-10-13 15:51:55 +0700

    update baseservlet class

---
+++ src/main/java/BaseServlet.java
@@ -0,0 +1,48 @@
+import Database.DatabaseConnector;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+
+
+public abstract class BaseServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private DatabaseConnector dbConnector = null;
+    private RequestDispatcher reqDispatcher = null;
+
+    protected BaseServlet() {
+
+    }
+
+    protected void createDatabaseConnectorInstance() {
+        dbConnector = DatabaseConnector.getInstance();
+        dbConnector.createConnection();
+    }
+
+    protected void destroyDatabaseConnectorInstance() {
+        dbConnector = null;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        dbConnector.closeConnection();
+        destroyDatabaseConnectorInstance();
+    }
+
+    protected DatabaseConnector getDatabaseConnectorInstance() {
+        if (dbConnector == null) {
+            dbConnector = DatabaseConnector.getInstance();
+            dbConnector.createConnection();
+        }
+        return dbConnector;
+    }
+
+    protected void setRequestDispatcher(RequestDispatcher reqDispatcher) {
+        this.reqDispatcher = reqDispatcher;
+    }
+
+    protected RequestDispatcher getRequestDispatcher() {
+        return reqDispatcher;
+    }
+}
