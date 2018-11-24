31a437282dea87ef2c23d4dd0c1c5cbcf7e2faf9
HOAPQ-LAPTOP\hoaph <20161743@student.hust.edu.vn>
2018-11-24 08:19:47 +0700

    fix database connector and change ui sweetbox

--- src/main/java/com/nhom17/database/DatabaseConnector.java
+++ src/main/java/com/nhom17/database/DatabaseConnector.java
@@ -13,7 +13,10 @@ public class DatabaseConnector {
 
     public Connection getConnection() {
         try {
-            connection = connection == null? DriverManager.getConnection(getConnectionUrl()) : connection;
+            if (connection == null)
+                connection = DriverManager.getConnection(getConnectionUrl());
+            else if (connection.isClosed())
+                connection = DriverManager.getConnection(getConnectionUrl());
         } catch (SQLException e) {
             e.printStackTrace();
         }
