70ac4b76445104e89da32123f1ea32d29e8e833a
HoaPhamHust <20161743@student.hust.edu.vn>
2019-11-05 10:58:07 +0700

    Tạo base

---
+++ app/controllers/home.js
@@ -0,0 +1,13 @@
+
+exports.home = function(req, res) {
+	res.render('index.ejs', {
+		error : req.flash("error"),
+		success: req.flash("success"),
+		session:req.session,
+		title: "Nhóm 04"
+	 });
+	 
+}
+
+
+    
