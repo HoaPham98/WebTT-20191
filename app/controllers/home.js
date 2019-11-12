e0c6c4cb13d1c539e2b539a08026b8b63817dcb7
Hong Nguyen <honggamxd@gmail.com>
2019-11-12 10:57:44 +0700

    Sua thu muc

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,48 @@
 
 exports.home = function(req, res) {
 	res.render('index.ejs', {
+		error: req.flash("error"),
+		success: req.flash("success"),
+		session: req.session,
+		title: "Nhóm 04"
+	});
+}
+	exports.schedule = function(req, res) {
+		res.render('schedule.ejs', {
+			error : req.flash("error"),
+			success: req.flash("success"),
+			session:req.session,
+			title: "Lịch diễn"
+		});
+	 
+}
+
+exports.performance = function(req, res) {
+	res.render('performance.ejs', {
 		error : req.flash("error"),
 		success: req.flash("success"),
 		session:req.session,
-		title: "Nhóm 04"
-	 });
-	 
+		title: "Vở diễn"
+	});
+
+}
+exports.news = function(req, res) {
+	res.render('news.ejs', {
+		error : req.flash("error"),
+		success: req.flash("success"),
+		session:req.session,
+		title: "Tin tức"
+	});
+
+}
+exports.contact = function(req, res) {
+	res.render('contact.ejs', {
+		error : req.flash("error"),
+		success: req.flash("success"),
+		session:req.session,
+		title: "Liên hệ"
+	});
+
 }
 
 
--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,10 @@ module.exports = function (app) {
 
     app.get('/', home.home);//home
     app.get('/home', home.home);//home
+    app.get('/schedule', home.schedule);
+    app.get('/performance', home.performance);
+    app.get('/news', home.news);
+    app.get('/contact', home.contact);
 
 
 }
